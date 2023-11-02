e6ae91d89e9f196f82a1899c145080c952ba6ab3
gtnoble <garretnoble@gmail.com>
2023-11-02 10:13:55 -0500

    Reorganized header

--- include/sinusoid.h
+++ include/sinusoid.h
@@ -17,18 +17,20 @@ Sinusoid sinusoid_make(double phase_degrees, double normalized_frequency);
 Sinusoid sinusoid_add(Sinusoid a, Sinusoid b);
 Sinusoid sinusoid_mult(Sinusoid a, Sinusoid b);
 Sinusoid sinusoid_div(Sinusoid a, Sinusoid b);
+Sinusoid sinusoid_normalize(Sinusoid x);
+Sinusoid sinusoid_negate_phase(Sinusoid x);
+Sinusoid sinusoid_shift_phase(double angle, Sinusoid x);
+
 double sinusoid_evaluate(Sinusoid x);
 double sinusoid_inphase(Sinusoid x);
 double sinusoid_quadrature(Sinusoid x);
 double sinusoid_amplitude(Sinusoid x);
 double sinusoid_phase(Sinusoid x);
 double sinusoid_angular_freq(Sinusoid x);
+
 double _Complex angular_to_complex_frequency(double angular_frequency);
 double complex_frequency_to_angular(double complex complex_frequency);
 double complex_frequency_to_ordinary(double complex complex_frequency);
-Sinusoid sinusoid_normalize(Sinusoid x);
-Sinusoid sinusoid_negate_phase(Sinusoid x);
-Sinusoid sinusoid_shift_phase(double angle, Sinusoid x);
 
 #endif
 
